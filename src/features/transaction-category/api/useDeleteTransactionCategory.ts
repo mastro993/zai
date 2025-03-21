56cdd6dfa41f392a58fc6e383ad29cbf15293f6a
Federico Mastrini <federicomastrini93@gmail.com>
2025-03-21 18:09:33 +0100

    fixes

--- src/features/transaction-category/api/useDeleteTransactionCategory.ts
+++ src/features/transaction-category/api/useDeleteTransactionCategory.ts
@@ -1,6 +1,6 @@
 import { db } from "@/lib/database";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import toast from "react-hot-toast";
+import { toast } from "sonner";
 import { TransactionCategory } from "../schema";
 
 export const useDeleteTransactionCategory = (category: TransactionCategory) => {
