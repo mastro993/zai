87845ad557c70a1d4384da460fb95c10c55861bd
Federico Mastrini <federicomastrini93@gmail.com>
2025-04-16 21:47:01 +0200

    zustand

--- src/routes/settings/index.tsx
+++ src/routes/settings/index.tsx
@@ -1,3 +1,4 @@
+import { useBankConnectionStore } from "@/features/bank-connection/store";
 import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/settings/")({
@@ -5,10 +6,13 @@ export const Route = createFileRoute("/settings/")({
 });
 
 function RouteComponent() {
+  const { firstName, setName } = useBankConnectionStore();
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-bold">Settings</h1>
       <Link to="/settings/playgrounds">Playgrounds</Link>
+      <p>{firstName}</p>
+      <button onClick={() => setName("Culon")}>Set Name</button>
       <Outlet />
     </div>
   );
