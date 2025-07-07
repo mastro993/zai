4af85eb15eeac87e536c674b78cbe50a28994cc9
Federico Mastrini <federicomastrini93@gmail.com>
2025-07-07 22:03:08 +0200

    wip: db

--- src-core/src/database/database_error.rs
+++ src-core/src/database/database_error.rs
@@ -7,4 +7,10 @@ pub enum DatabaseError {
         path: String,
         source: std::io::Error,
     },
+    #[error("Failed to connect to the database: {0}")]
+    ConnectionPool(String),
+    #[error("Database migration failed: {0}")]
+    MigrationFailed(String),
+    #[error("Failed to create database connection pool: {0}")]
+    PoolCreationFailed(#[from] r2d2::Error),
 }
